8c2e3fba396024fa2d5e44bab015a87b8b9677b9
Sabit Nepal <gta0004@gmail.com>
2016-02-07 12:00:14 -0800

    Moved most of index page to jade templates. Updated Boostrap and Jquery to latest versions.

--- server.js
+++ server.js
@@ -1,31 +1,77 @@
+
 /**
- * Introduction to Human-Computer Interaction
- * Lab 2
- * --------------
- * Created by: Michael Bernstein
- * Last updated: December 2013
+ * Module dependencies.
  */
-var PORT = 3000;
 
-// Express is a web framework for node.js
-// that makes nontrivial applications easier to build
 var express = require('express');
+var http = require('http');
+var path = require('path');
 
-// Create the server instance
+// Example route
+// var user = require('./routes/user');
+
 var app = express();
 
-// Print logs to the console and compress pages we send
-app.use(express.logger());
-app.use(express.compress());
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('Intro HCI secret key'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Return all pages in the /static directory
-// whenever they are requested at '/'
-// e.g., http://localhost:3000/index.html
-// maps to /static/index.html on this machine
-app.use(express.static(__dirname + '/static'));
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
 
-// Start the server
-var port = process.env.PORT || PORT; // 80 for web, 3000 for development
-app.listen(port, function() {
-	console.log("Node.js server running on port %s", port);
+// Add routes here
+app.get('/', function(req, res) {
+  res.render('index', {
+    title: 'Ekceltricity',
+    households: [
+      {
+        'name': 'College Household', 
+        'appliances': [
+          {
+            'name': 'Laptop Charger',
+            'usage': 5.43,
+            'status': 1
+          },
+          {
+            'name': 'Lamp',
+            'usage': 0.26,
+            'status': 1
+          },
+          {
+            'name': 'Phone Charger',
+            'usage': 1.98,
+            'status': 1
+          },
+          {
+            'name': 'A/C',
+            'usage': 8.26,
+            'status': 0
+          },
+          {
+            'name': 'Fan',
+            'usage': 1.26,
+            'status': 0
+          }
+        ]
+      }
+    ],    
+  });
+});
+// Example route
+// app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
