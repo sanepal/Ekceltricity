528a6727ecf5d7fffe2531cbb688c4b0ca03dcc3
Sabit Nepal <gta0004@gmail.com>
2016-02-10 17:33:27 -0800

    Apparently this was invalid syntax

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var db = require('../db');
 exports.view = function(req, res) {
   var households = db.getHouseholds();
   res.render('index', {
-    title: 'Ekceltricity',
-    households
+    'title': 'Ekceltricity',
+    'households': households
   });
 }
