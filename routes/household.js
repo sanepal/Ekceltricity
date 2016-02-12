a3f299dfdd95cda1a21f65d2aa0206c7099cb9f7
Sabit Nepal <gta0004@gmail.com>
2016-02-12 08:31:38 -0800

    Redirect to appliance page after making household. Able to create household with no other members

--- routes/household.js
+++ routes/household.js
@@ -9,19 +9,21 @@ exports.create = function(req, res) {
   var members = [];
 
   // Create new users or find the existing ones
-  household.members.forEach(function(email) {
-    if (email === '') {
-      return;
-    }
-    var member = db.createOrGetUser(email);
-    members.push({'id': member.id, 'appliances': []});
-  });
+  if (household.members.length !== 0) {
+    household.members.forEach(function(email) {
+      if (email === '') {
+        return;
+      }
+      var member = db.createOrGetUser(email);
+      members.push({'id': member.id, 'appliances': []});
+    });
+  }
 
   // Add current user to household
   members.push({'id': req.userId, 'appliances': []});
   household.members = members;
-  db.createHousehold(household);
+  var id = db.createHousehold(household);
 
   // TODO: Redirect to my appliances page for this household 
-  res.redirect('/');
+  res.redirect('/breakdown/' + id + '/' + req.userId);
 }
