4300c0055456024fb2d57f92c8d8c777fc0d85f3
Dan Thai <dqthai@ucsd.edu>
2016-02-18 11:51:09 -0800

    reorganized code

--- routes/sign-in.js
+++
@@ -1,8 +0,0 @@
-var db = require('../db');
-
-exports.view = function(req, res) {
-
-  res.render('sign-in', {
-    'title': 'Ekceltricity'
-  });
-}
--- routes/welcome.js
+++
@@ -1,8 +0,0 @@
-var db = require('../db');
-
-exports.view = function(req, res) {
-
-  res.render('welcome', {
-    'title': 'Ekceltricity'
-  });
-}
