5f83dbe71260271cb83c53c393300eb2efeea2ee
Rahul <rss002>
2016-02-18 23:33:20 -0800

    add full data and chart integration!!!@!@!

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var db = require('../db');
+var breakdown = require('../routes/breakdown');
 
 exports.view = function(req, res) {
   var households = db.getHouseholds();
-
+  var applianceBreakdowns = [];
   households.forEach(function(h) {
     h.appliances = [];
     h.members.forEach(function(m) {
@@ -10,12 +11,17 @@ exports.view = function(req, res) {
         m.appliances.forEach(function(a) {
           h.appliances.push(db.getAppliance(a));
         });
+        console.log("h id: " + h.id + ", " + req.userId);
+        var memberUsageData = breakdown.getUserUsageData(h, req.userId);
+        console.log("applianceCosts: " + JSON.stringify(memberUsageData.applianceMonthCosts));
+        applianceBreakdowns.push({'householdName':h.name, 'applianceCosts': memberUsageData.applianceMonthCosts});
       }
     });
   });
 
   res.render('index', {
     'title': 'Ekceltricity',
-    'households': households
+    'households': households,
+    'applianceBreakdowns': applianceBreakdowns
   });
 }
