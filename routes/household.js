ba6537097c1e79452cc0399499910745617b1bc6
Sabit Nepal <gta0004@gmail.com>
2016-02-18 20:50:09 -0800

    Fixed saving household options so existing member's appliances are not reset

--- routes/household.js
+++ routes/household.js
@@ -42,17 +42,27 @@ exports.viewOptions = function(req, res) {
 }
 
 exports.update = function(req, res) {
-  var household = {};
-  var members = [];
+  var householdId = req.body.id;
+  var existingHousehold = db.getHousehold(householdId);
+
+  var currentMembers = existingHousehold.members;
   var existingMembers = req.body.existingMembers;
   var newMembers = req.body.newMembers;
+  var members = [];
 
-  // Create new users or find the existing ones
-  // { householdName: 'College Household',
-  // id: '0',
-  // rate: '16.35',
-  // existingMembers: [ '0', '1', '2', '3' ],
-  // newMembers: [ '' ] }
+  // Merge the existing users 
+  if (existingMembers !== undefined) {
+    for (var i = 0; i < existingMembers.length; i++) {
+      var member = db.getUser(existingMembers[i]);
+      for (var j = 0; j < currentMembers.length; j++) {
+        if (member.id == currentMembers[j].id) {
+          members.push(currentMembers[j]);
+        }
+      }
+    }
+  }
+
+  // Add the new members
   newMembers.forEach(function(email) {
     if (email === '') {
       return;
@@ -61,22 +71,12 @@ exports.update = function(req, res) {
     members.push({'id': member.id, 'appliances': []});
   });
 
-  existingMembers.forEach(function(email) {
-    if (email === '') {
-      return;
-    }
-    var member = db.getUser(email);
-    members.push({'id': member.id, 'appliances': []});
-  });
-
-  // Add current user to household
-  // members.push({'id': userId, 'appliances': []});
+  var household = {};
   household.members = members;
   household.id = req.body.id;
   household.rate = req.body.rate;
   household.name = req.body.householdName;
   db.editHousehold(household);
 
-  // TODO: Redirect to my appliances page for this household 
   res.redirect('/breakdown/' + household.id);
 }
