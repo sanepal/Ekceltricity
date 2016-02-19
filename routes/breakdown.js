4ea6c3dcceb18b73efb94ad5081483faf1df645a
Rahul <rss002@ucsd.edu>
2016-02-18 16:30:20 -0800

    parts of data link to the graphs

--- routes/breakdown.js
+++ routes/breakdown.js
@@ -1,5 +1,13 @@
 var db = require('../db.js');
 
+function getTotalUsageThisMonth(var household) {
+
+}
+
+function millisToHours(millis) {
+    return millis / 1000 / 60 / 60;
+}
+
 exports.view = function(req, res) {
 	var householdId = req.params.household;
 	var household = db.getHousehold(householdId);
@@ -15,7 +23,6 @@ exports.view = function(req, res) {
 }
 
 exports.viewUser = function(req, res) {
-    console.log("IN my-overview.js.view with memberId: " + req.params.member);
     var memberId = req.params.member;
     var householdId = req.params.household;
 
@@ -23,18 +30,38 @@ exports.viewUser = function(req, res) {
     var member = undefined;
 
     var appliances = [];
-  
+    // values are {'name', 'totalUsage'} tuples which have usage for each appliance, e.g. {'Laptop', '1.23'}
+    var memberUsageAmounts = [];
+    var totalUsageAmounts = [0, 0, 0, 0, 0, 0, 0];
     household.members.forEach(function(m) {
         if (m.id == memberId) {
             member = db.getUser(m.id);
             if (m.appliances !== undefined) {
                 m.appliances.forEach(function(a) {
-                    appliances.push(db.getAppliance(a));
+                    var appliance = db.getAppliance(a);
+                    appliances.push(appliance);
+                    var totalUsage = 0;
+                    for (var i = 0; i < appliance.usage.length; i++) {
+                        var startDate = new Date(appliance.usage[i].start);
+                        if (appliance.usage[i].end == null) {
+                            totalUsage += ((Date.now() - startDate) / 1000 / 60 / 60) * appliance.rate;
+                            continue;
+                        }
+                        var timeInMillis = appliance.usage[i].end - appliance.usage[i].start;
+                        if (timeInMillis != undefined) {
+                            totalUsage += (timeInMillis / 1000 / 60 / 60) * appliance.rate;
+                        }
+                    }
+                    memberUsageAmounts.push({"applianceName":appliance.name, "totalUsage":totalUsage});
                 });
             }
         }
-  });
+        // add total usage across all household members
+        m.appliances.forEach(function(applianceId) {
 
-  var title = household.name + " > " + (memberId == req.userId ? 'My Usage' : member.name + "'s Usage");
-  res.render('my-overview', {'title': title, 'household': household, 'member': member, 'appliances': appliances});
+        })
+    });
+    console.log("Sending memberUsageAmounts: " + JSON.stringify(memberUsageAmounts));
+    var title = household.name + " > " + (memberId == req.userId ? 'My Usage' : member.name + "'s Usage");
+    res.render('my-overview', {'title': title, 'household': household, 'member': member, 'appliances': appliances, 'memberUsageAmounts':memberUsageAmounts});
 }
